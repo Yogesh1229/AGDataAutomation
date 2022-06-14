90a9fbdeb7f2a2229a9a20101cdfa30030d2be03
Git Demo <CICDDemo2020@outlook.com>
2022-06-14 15:33:02 +0530

    Created abstract TestCaseSetup class to setup and dispose the test case.

---
+++ AGDataUI/Tests/Base/TestCaseSetup.cs
@@ -0,0 +1,25 @@
+﻿using AGDataUI.Helpers.NonPageSpecificHelpers;
+using OpenQA.Selenium;
+using System;
+
+namespace AGDataUI.Tests.Base
+{
+    public abstract class TestCaseSetup : IDisposable
+    {
+        protected IWebDriver driver;
+
+        public TestCaseSetup()
+        {
+            driver = BrowserHelper.InitializeDriver("Chrome");
+            SeleniumHelper.NavigateToUrl(driver, "https://www.agdata.com/");
+        }
+
+        public void Dispose()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
--- AGDataUI/Tests/JobOpenings/ManagerOpeningSearch.cs
+++ AGDataUI/Tests/JobOpenings/ManagerOpeningSearch.cs
@@ -1,21 +1,14 @@
 ﻿using AGDataUI.Helpers.NonPageSpecificHelpers;
 using AGDataUI.Helpers.PageSpecificHelpers;
 using AGDataUI.Models.ObjectsRepository;
+using AGDataUI.Tests.Base;
 using OpenQA.Selenium;
 using Xunit;
 
 namespace AGDataUI.Tests.JobOpenings
 {
-    public class ManagerOpeningSearch
+    public class ManagerOpeningSearch : TestCaseSetup
     {
-        IWebDriver driver;
-
-        public ManagerOpeningSearch()
-        {
-            driver = BrowserHelper.InitializeDriver("Chrome");
-            SeleniumHelper.NavigateToUrl(driver, "https://www.agdata.com/");
-        }
-
         [Fact]
         [Trait("UI", "ManagerOpeningSearch")]
         public void AGData_UI_ManagerOpeningSearch_SuccessfulRequests_01_01_ClickOnSecondLinkContainsManagerFromAllAvailableJobs()
